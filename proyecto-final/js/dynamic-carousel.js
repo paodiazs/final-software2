5ce520f5c904d651c0344afedeba710be46d057b
luigijimha <113155381+luigijimha@users.noreply.github.com>
2023-05-21 23:33:09 -0600

    info pages added
    added info for descriptive pages, fixed carousel errors and added hyperlink buttons that tell the user how to get to the pages with info.

--- proyecto-final/js/dynamic-carousel.js
+++ proyecto-final/js/dynamic-carousel.js
@@ -1,10 +1,13 @@
 // title and text of the document
 let title = document.getElementById("content-title");
 let text = document.getElementById("content-text");
+let contact_title = document.getElementById("contact-title");
 let contact_phone = document.getElementById("contact-phone");
 let contact_mail = document.getElementById("contact-mail");
+let schedule_title = document.getElementById("schedule-title");
 let schedule = document.getElementById("schedule");
 let map_link = document.getElementById("map-link");
+let special_link = document.getElementById("special-link");
 
 // variable used for button cooldown
 let buttonCooldown = false;
@@ -32,7 +35,7 @@ let data;
 previous_button.addEventListener("click", function() {
     if (!buttonCooldown) {
         contentID = (contentID == 0 ? data.length-1 : contentID-1);
-        updateData()
+        updateData();
         cooldown();
     }
 });
@@ -41,7 +44,7 @@ previous_button.addEventListener("click", function() {
 next_button.addEventListener("click", function() {
     if (!buttonCooldown) {
         contentID = (parseInt(contentID) + 1) % data.length;
-        updateData()
+        updateData();
         cooldown();
     }
 });
@@ -50,6 +53,12 @@ next_button.addEventListener("click", function() {
 function updateData() {
     title.textContent = data[contentID][0];
     text.textContent = data[contentID][1];
+
+    if(data[contentID][2] == "" && data[contentID][3] == "") {
+        contact_title.innerHTML = "<strong><small>Te invitamos a visitar nuestras insatalaciones en el mapa</small></strong>";
+    } else {
+        contact_title.innerHTML = "<strong><small>Contacto</small></strong>";
+    }
 
     if(data[contentID][2] != "") {
         contact_phone.innerHTML = '<img src="./assets/icons/whatsapp.svg" alt="WhatsApp" class="contact-icons"><strong> ' + data[contentID][2] + '</strong>';
@@ -62,9 +71,26 @@ function updateData() {
     } else {
         contact_mail.textContent = "";
     }
-        
+    
+    if(data[contentID][4] == "") {
+        schedule_title.innerHTML = "<strong><small>Nuestras instalaciones estan abiertas siempre que desees utilizarlas.</small></strong>"
+    } else {
+        schedule_title.innerHTML = "<strong><small>Horario</small></strong>";
+    }
     schedule.textContent = data[contentID][4];
-    // map_link.setAttribute("href", data[contentID][5]);
+
+    if(data[contentID][6] != "") {
+        special_link.innerHTML = data[contentID][6];
+    } else {
+        special_link.textContent = "";
+    }
+
+    if(data[contentID][5] == "#") {
+        map_link.innerHTML = "";
+    } else {
+        map_link.innerHTML = '<a class="btn btn-success btn-block" style="background-color: #fa6400" onclick="mapDirection(\'' + data[contentID][5] + '\')"><p class="lead text-light" style="margin: 7px 0;"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i> Ver ubicación en el mapa</p></a>';
+    }
+    
 }
 
 // listeners for all carousel buttons
@@ -78,4 +104,10 @@ for (let i = 0; i < slide_buttons.length; i++) {
             cooldown();
         }
     });
+}
+
+// redirect user to map destination
+function mapDirection(destiny) {
+    const url = 'mapaUniversidadCompleto.html?final_node=' + encodeURIComponent(destiny);
+    window.location.href = url;
 }
